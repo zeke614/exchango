b2fdb804b84ae30ef1d393fd07edd459623c6067
zeke614 <ezekielarkohamissah@gmail.com>
2025-06-12 17:24:14 -0800

    SEO

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./components/footer";
 import CurrencyDropdown from "./components/currencies";
 import countriesData from "./components/data";
 import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
 //import { motion } from "framer-motion";
 
 function App() {
@@ -170,126 +171,218 @@ function App() {
 
   if (!isMobile) {
     return (
-      <div className="min-h-screen flex items-center justify-center text-center p-6 bg-black text-white">
-        <div className="text-xl">
-          <h1 className="mb-2 text-2xl">
-            The site is only accessible on mobile screens.
-          </h1>
-          <p className="flex items-center justify-center gap-1">
-            Kindly visit on your mobile phone.{" "}
-            <i className="bx bx-mobile-alt text-2xl"></i>
-          </p>
-          <p className="flex items-center justify-center gap-1">
-            Thank you. <i className="bx bx-smile text-2xl"></i>
-          </p>
+      <>
+        <Helmet>
+          <title>exchango – Smart Currency Converter</title>
+          <meta
+            name="description"
+            content="Convert currencies with real-time rates and a clean, mobile-first interface."
+          />
+          <meta
+            name="keywords"
+            content="currency converter, money exchange, exchango, exchange rates, convert currencies online"
+          />
+          <meta name="author" content="exchango" />
+
+          <meta
+            property="og:title"
+            content="exchango – Smart Currency Converter"
+          />
+          <meta
+            property="og:description"
+            content="Convert currencies with real-time rates and a clean, mobile-first interface."
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://exchangoio.vercel.app" />
+          {/*<meta
+            property="og:image"
+            content="https://exchangoio.vercel.app/preview.png"
+          />*/}
+
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:title"
+            content="exchango – Smart Currency Converter"
+          />
+          <meta
+            name="twitter:description"
+            content="Convert currencies with real-time rates and a clean, mobile-first interface."
+          />
+          {/*<meta
+            name="twitter:image"
+            content="https://exchangoio.vercel.app/preview.png"
+          />*/}
+
+          <link rel="canonical" href="https://exchangoio.vercel.app" />
+        </Helmet>
+
+        <div className="min-h-screen flex items-center justify-center text-center p-6 bg-black text-white">
+          <div className="text-xl">
+            <h1 className="mb-2 text-2xl">
+              The site is only accessible on mobile screens.
+            </h1>
+            <p className="flex items-center justify-center gap-1">
+              Kindly visit on your mobile phone.{" "}
+              <i className="bx bx-mobile-alt text-2xl"></i>
+            </p>
+            <p className="flex items-center justify-center gap-1">
+              Thank you. <i className="bx bx-smile text-2xl"></i>
+            </p>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 
   return (
-    <div className="min-h-screen flex flex-col bg-[#fefefe] text-black">
-      <Header />
-      <main className="flex-grow px-4">
-        <div className="pb-12 pt-16">
-          <h2 className="text-center text-[21px] mb-6">
-            {t("welcome.welcomeLine1")}
-            <span className="font-medium text-[22px]">
-              exchan<span className="text-[#256F5C]">go</span>,
-            </span>
-            <br />
-            {t("welcome.welcomeLine2")}
-          </h2>
+    <>
+      <Helmet>
+        <title>exchango – Smart Currency Converter</title>
+        <meta
+          name="description"
+          content="Convert currencies with real-time rates and a clean, mobile-first interface."
+        />
+        <meta
+          name="keywords"
+          content="currency converter, money exchange, exchango, exchange rates, convert currencies online"
+        />
+        <meta name="author" content="exchango" />
 
-          <section id="converter" className="pt-12">
-            <div className="bg-white px-3.5 py-6 border border-gray-200 rounded-2xl flex flex-col items-center gap-3 shadow-md">
-              <h3 className="text-center text-[26px] text-[#256F5C] font-medium my-3">
-                {t("converterWords.title")}
-              </h3>
+        <meta
+          property="og:title"
+          content="exchango – Smart Currency Converter"
+        />
+        <meta
+          property="og:description"
+          content="Convert currencies with real-time rates and a clean, mobile-first interface."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://exchangoio.vercel.app" />
+        {/*<meta
+            property="og:image"
+            content="https://exchangoio.vercel.app/preview.png"
+          />*/}
 
-              <div className="w-full max-w-xs">
-                <label className="block text-end text-[17px] text-gray-600 mb-1.5">
-                  {t("converterWords.amount")}
-                </label>
-                <div className="flex items-center justify-between border gap-6 border-gray-200 rounded-xl px-4 py-[17px] bg-white shadow-sm">
-                  <CurrencyDropdown
-                    selected={fromCurrency}
-                    setSelected={setFromCurrency}
-                  />
-                  <div className="w-[52%]">
-                    <input
-                      type="number"
-                      value={amount}
-                      onChange={(e) => setAmount(e.target.value)}
-                      className="outline-none border-none w-full bg-transparent text-xl text-end font-medium appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-                      placeholder="0.00"
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content="exchango – Smart Currency Converter"
+        />
+        <meta
+          name="twitter:description"
+          content="Convert currencies with real-time rates and a clean, mobile-first interface."
+        />
+        {/*<meta
+            name="twitter:image"
+            content="https://exchangoio.vercel.app/preview.png"
+          />*/}
+
+        <link rel="canonical" href="https://exchangoio.vercel.app" />
+      </Helmet>
+
+      <div className="min-h-screen flex flex-col bg-[#fefefe] text-black">
+        <Header />
+        <main className="flex-grow px-4">
+          <div className="pb-12 pt-16">
+            <h2 className="text-center text-[21px] mb-6">
+              {t("welcome.welcomeLine1")}
+              <span className="font-medium text-[22px]">
+                exchan<span className="text-[#256F5C]">go</span>,
+              </span>
+              <br />
+              {t("welcome.welcomeLine2")}
+            </h2>
+
+            <section id="converter" className="pt-12">
+              <div className="bg-white px-3.5 py-6 border border-gray-200 rounded-2xl flex flex-col items-center gap-3 shadow-md">
+                <h3 className="text-center text-[26px] text-[#256F5C] font-medium my-3">
+                  {t("converterWords.title")}
+                </h3>
+
+                <div className="w-full max-w-xs">
+                  <label className="block text-end text-[17px] text-gray-600 mb-1.5">
+                    {t("converterWords.amount")}
+                  </label>
+                  <div className="flex items-center justify-between border gap-6 border-gray-200 rounded-xl px-4 py-[17px] bg-white shadow-sm">
+                    <CurrencyDropdown
+                      selected={fromCurrency}
+                      setSelected={setFromCurrency}
                     />
+                    <div className="w-[52%]">
+                      <input
+                        type="number"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        className="outline-none border-none w-full bg-transparent text-xl text-end font-medium appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                        placeholder="0.00"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              {isLoading ? (
-                <div className="flex items-center justify-center animate-spin mt-6 mb-4 text-[#256F5C]">
-                  <span className="material-symbols-outlined text-[40px]">
-                    currency_exchange
-                  </span>
-                </div>
-              ) : (
-                <button
-                  onClick={handleSwap}
-                  className="text-[26px] my-7 items-center bg-[#256F5C] rounded-full rotate-90 p-2 justify-center flex"
-                  title="Swap currencies"
-                >
-                  <span className="material-symbols-outlined text-white">
-                    sync_alt
-                  </span>
-                </button>
-              )}
-
-              <div className="w-full max-w-xs">
-                <label className="block text-end text-[17px] text-gray-600 mb-2">
-                  {t("converterWords.convertedFigure")}
-                </label>
-                <div className="flex items-center justify-between gap-6 border border-gray-200 rounded-xl px-4 py-[17px] bg-white shadow-sm">
-                  <CurrencyDropdown
-                    selected={toCurrency}
-                    setSelected={setToCurrency}
-                  />
-                  <div className="w-[52%]">
-                    <input
-                      type="text"
-                      value={convertedAmount}
-                      readOnly
-                      className="outline-none border-none w-full bg-transparent text-xl text-end font-medium"
-                    />
+                {isLoading ? (
+                  <div className="flex items-center justify-center animate-spin mt-6 mb-4 text-[#256F5C]">
+                    <span className="material-symbols-outlined text-[40px]">
+                      currency_exchange
+                    </span>
                   </div>
-                </div>
-              </div>
-
-              <div>
-                <h5 className="text-center text-2xl font-medium mt-12">
-                  {t("converterWords.rate")}
-                </h5>
-                <p className="text-center text-[22px] font-medium text-gray-600 mt-1.5">
-                  {rate
-                    ? `${fromCurrency.symbol}1.00 ${fromCurrency.code} = ${
-                        toCurrency.symbol
-                      }${rate.toFixed(4)} ${toCurrency.code}`
-                    : "Could not fetch rate."}
-                </p>
-
-                {rate && relativeTime && (
-                  <p className="text-center text-lg font-light text-gray-600 mt-4 mb-2">
-                    {relativeTime}
-                  </p>
+                ) : (
+                  <button
+                    onClick={handleSwap}
+                    className="text-[26px] my-7 items-center bg-[#256F5C] rounded-full rotate-90 p-2 justify-center flex"
+                    title="Swap currencies"
+                  >
+                    <span className="material-symbols-outlined text-white">
+                      sync_alt
+                    </span>
+                  </button>
                 )}
+
+                <div className="w-full max-w-xs">
+                  <label className="block text-end text-[17px] text-gray-600 mb-2">
+                    {t("converterWords.convertedFigure")}
+                  </label>
+                  <div className="flex items-center justify-between gap-6 border border-gray-200 rounded-xl px-4 py-[17px] bg-white shadow-sm">
+                    <CurrencyDropdown
+                      selected={toCurrency}
+                      setSelected={setToCurrency}
+                    />
+                    <div className="w-[52%]">
+                      <input
+                        type="text"
+                        value={convertedAmount}
+                        readOnly
+                        className="outline-none border-none w-full bg-transparent text-xl text-end font-medium"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <h5 className="text-center text-2xl font-medium mt-12">
+                    {t("converterWords.rate")}
+                  </h5>
+                  <p className="text-center text-[22px] font-medium text-gray-600 mt-1.5">
+                    {rate
+                      ? `${fromCurrency.symbol}1.00 ${fromCurrency.code} = ${
+                          toCurrency.symbol
+                        }${rate.toFixed(4)} ${toCurrency.code}`
+                      : "Could not fetch rate."}
+                  </p>
+
+                  {rate && relativeTime && (
+                    <p className="text-center text-lg font-light text-gray-600 mt-4 mb-2">
+                      {relativeTime}
+                    </p>
+                  )}
+                </div>
               </div>
-            </div>
-          </section>
-        </div>
-      </main>
-      <Footer />
-    </div>
+            </section>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
