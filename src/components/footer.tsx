65a5ddc4bbd69e51859472979c0ab99590063207
zeke614 <ezekielarkohamissah@gmail.com>
2025-06-14 15:36:35 -0800

    UI

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -95,7 +95,7 @@ export default function Footer() {
           <a
             href="https://github.com/zeke614"
             target="_blank"
-            className="font-medium ml-1 text-gray-800 underline"
+            className="font-medium ml-1 text-[#256F5C] text-[1.09375rem] underline underline-offset-2"
           >
             Ezekiel
           </a>
